67b771c37421aa6efbb11e134590665a7a3d7cb4
Kat Reinhart <kat.reinhart@gmail.com>
2018-01-24 11:49:12 -0800

    Perform workout interaction working decently

--- PUSH_IT/Controller/PerformWorkoutVC.swift
+++ PUSH_IT/Controller/PerformWorkoutVC.swift
@@ -10,9 +10,58 @@ import UIKit
 
 class PerformWorkoutVC: UIViewController {
 
+    // Outlets
     @IBOutlet weak var menuBtn: UIButton!
+    @IBOutlet weak var exerciseNameLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var repsLbl: UILabel!
+    @IBOutlet weak var setsLbl: UILabel!
+    
+    @IBOutlet weak var currentSetLbl: UILabel!
+    
+    @IBOutlet weak var repsDoneLbl: UILabel!
+    
+    @IBOutlet weak var InputWorkoutView: UIView!
+    @IBOutlet weak var repsSlider: UISlider!
+    
+    // Variables
+    var currentExercise: String = ""
+    var targetWeight: Int = 0
+    var targetReps: Int = 0
+    var targetSets: Int = 0
+    
+    var exerciseIndex: Int = 0
+    var setIndex: Int = 0
+    
+    var repsPerformed: Int = 0
+    
+    var repsDoneLblText = ""
+    var currentSetLblText = ""
+
+    
+    // Life cycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // load current exercise stats
+        currentExercise = WorkoutDataService.instance.activeWorkout!.exercises[0].type
+        targetWeight = WorkoutDataService.instance.activeWorkout!.exercises[0].goalWeight
+        targetReps = WorkoutDataService.instance.activeWorkout!.exercises[0].goalRepsPerSet
+        targetSets = WorkoutDataService.instance.activeWorkout!.exercises[0].goalSets
+        
+        // Load data into view
+        exerciseNameLbl.text = currentExercise
+        weightLbl.text = String(targetWeight)
+        repsLbl.text = String(targetReps)
+        setsLbl.text = String(targetSets)
+        
+        // set up sets and reps labels
+        currentSetLblText = "Set \(setIndex + 1) of \(targetSets)"
+        currentSetLbl.text = currentSetLblText
+        repsDoneLblText = "\(repsPerformed) out of \(targetReps)"
+        repsDoneLbl.text = repsDoneLblText
+        
+        InputWorkoutView.isHidden = true
         
         // Menu button stuff
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
@@ -21,4 +70,39 @@ class PerformWorkoutVC: UIViewController {
     }
     
     
+    
+    // Actions
+    
+    @IBAction func GoBtnPressed(_ sender: Any) {
+        InputWorkoutView.isHidden = false
+        
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: Any) {
+        repsPerformed = Int(repsSlider.value)
+        repsDoneLblText = "\(repsPerformed) out of \(targetReps)"
+        repsDoneLbl.text = repsDoneLblText
+        
+    }
+    @IBAction func nextBtnPressed(_ sender: Any) {
+        
+        setIndex += 1
+        
+        if setIndex == targetSets {
+            exerciseIndex += 1
+            if exerciseIndex == WorkoutDataService.instance.activeWorkout!.exercises.count {
+                debugPrint("You're done! Booyah.")
+                return
+            }
+            else {
+                setIndex = 0
+                targetSets = WorkoutDataService.instance.activeWorkout!.exercises[exerciseIndex].goalSets
+                currentExercise = WorkoutDataService.instance.activeWorkout!.exercises[exerciseIndex].type
+                exerciseNameLbl.text = currentExercise
+            }
+        }
+        
+        currentSetLblText = "Set \(setIndex + 1) of \(targetSets)"
+        currentSetLbl.text = currentSetLblText
+    }
 }
--- PUSH_IT/Services/WorkoutDataService.swift
+++ PUSH_IT/Services/WorkoutDataService.swift
@@ -76,27 +76,6 @@ class WorkoutDataService {
         }
     }
     
-    func addExerciseToWorkout(workout: Workout, targetWeight: Int, exerciseName: String, exerciseReps: Int, exerciseSets: Int) {
-        
-        let body = [
-            "exercise_name": exerciseName,
-            "goal_sets": exerciseSets,
-            "goal_reps": exerciseReps
-            ] as [String : Any]
-        
-        let newExercise = Exercise(type: exerciseName, goalWeight: targetWeight, goalSets: exerciseSets, goalRepsPerSet: exerciseReps, sets: [Set]())
-        
-        activeWorkout?.exercises.append(newExercise)
-        
-        Alamofire.request("\(BASE_URL)/workouts/\(activeWorkoutID!)/exercises", method: .post, parameters: body, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
-            if response.result.error == nil {
-                debugPrint("Something worked")
-            } else {
-                debugPrint("something didn't work")
-            }
-        }
-    }
-    
     func performSet(repsAttempted: Int, repsCompleted: Int) {
         
     }
