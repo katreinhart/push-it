095a428dd184e73bacd6486fb451c13fa6d3a8e2
Kat Reinhart <kat.reinhart@gmail.com>
2018-01-30 15:48:41 -0800

    Add gradient view and rounded button classes and apply to storyboard

---
+++ PUSH_IT/View/GradientView.swift
@@ -0,0 +1,36 @@
+//
+//  GradientView.swift
+//  PUSH_IT
+//
+//  Created by Katherine Reinhart on 1/30/18.
+//  Copyright © 2018 reinhart.digital. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.1907421947, green: 0.380954206, blue: 0.5554463267, alpha: 1) {
+        didSet{
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 1, green: 0.3882352941, blue: 0.6588235294, alpha: 1) {
+        didSet{
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0 )
+    }
+    
+}
---
+++ PUSH_IT/View/RoundedButton.swift
@@ -0,0 +1,33 @@
+//
+//  RoundedButton.swift
+//  PUSH_IT
+//
+//  Created by Katherine Reinhart on 1/30/18.
+//  Copyright © 2018 reinhart.digital. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
+
